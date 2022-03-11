8fd416724cf8f7fc43fc0491a2b17f54fbbc9650
RedShirt1142 <red.perc.shirt@icloud.com>
2022-03-11 11:07:19 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
-        if(\App::environment(['production'])){
+        // production→本番環境  develop→テスト環境
+        if(\App::environment(['production'])||\App::environment(['develop'])){
             \URL::forceScheme('https');
         }
     }
